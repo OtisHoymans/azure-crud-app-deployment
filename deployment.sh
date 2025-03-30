181252a0352884e56308268e9e7cc27af9cd4a3d
OtisHoymans <r0984415@student.thomasmore.be>
2025-03-30 17:21:55 +0200

    Adjusted deployment.sh order of commands

--- deployment.sh
+++ deployment.sh
@@ -1,21 +1,26 @@
-#Run this file with 'sh deployment.sh' to fully deploy the application on Azure.
+#!/bin/bash
 
-#Create a resource group where our resources will be stored. 
+# Create a resource group where our resources will be stored. 
 az group create --name crudapp-rg --location eastus  
 
-#Create the ACR by deploying the acr.bicep template. 
+# Create the ACR by deploying the acr.bicep template.
 az deployment group create --resource-group crudapp-rg --template-file acr.bicep
 
-# Log in to ACR so the image can be built and pushed to it. 
+# Get the ACR login server and name after the deployment is successful
+ACR_LOGIN_SERVER=$(az deployment group show --resource-group crudapp-rg --name $(az deployment group list --resource-group crudapp-rg --query "[0].name" -o tsv) --query "properties.outputs.acrLoginServer.value" -o tsv)
+
+ACR_NAME=$(az deployment group show --resource-group crudapp-rg --name $(az deployment group list --resource-group crudapp-rg --query "[0].name" -o tsv) --query "properties.outputs.acrName.value" -o tsv)
+
+# Log in to ACR so the image can be built and pushed to it.
 az acr login --name $ACR_NAME
 
-# Build and push the image. 
+# Build and push the Docker image to ACR.
 docker build -t $ACR_LOGIN_SERVER/crud-flask-app:v1 .
 docker push $ACR_LOGIN_SERVER/crud-flask-app:v1
 
-#The ACR login server and name.  
+# Display the ACR login server and name.
 echo "ACR Login Server: $ACR_LOGIN_SERVER"
 echo "ACR Name: $ACR_NAME"
 
-#Deploy the 'main.bicep' template, which deploys 'infra.bicep' and 'app.bicep' to finally deploy the application on Azure. 
-az deployment group create --resource-group crudapp-rg --template-file main.bicep --parameters acrLoginServer=acroh.azurecr.io 
+# Deploy the 'main.bicep' template, which deploys 'infra.bicep' and 'app.bicep' to finally deploy the application on Azure. 
+az deployment group create --resource-group crudapp-rg --template-file main.bicep --parameters acrLoginServer=$ACR_LOGIN_SERVER
